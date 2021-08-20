5a21a97d01cf85e7d5ae16634d7272f0fab869cb
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-20 15:18:47 +0530

    Adding UC3 Partime Worker Added

--- EmployWage.java
+++ EmployWage.java
@@ -1,26 +1,28 @@
 package com.employeeWage;
 
 public class EmployWage {
+	public static final int IS_Part_Time = 1;// @param IS_Part_Time is used to check job status
+	public static final int WAGE_PER_HR =20; // @param WAGE_PER_HR is used to assign wage per hour
+	public static final int IS_FULL_TIME = 2; // @param IS_FULL_TIME is used to check job status
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employ Wage");
-		
-		int WAGE_PER_HR =20; // @param WAGE_PER_HR is used to assign wage per hour
-        int FULL_WORKING_DAY =8; // @param IS_FULL_TIME is used to assign full working day time
-		int IS_FULL_TIME = 1; // @param IS_FULL_TIME is used to check job status
+
 		int empHrs =0;
         int empWage = 0;
 		
 		// Computation
-		double empcheck = Math.floor(Math.random() * 10) % 2;
+		double empcheck = Math.floor(Math.random() * 10) % 3;
 		
-		if (empcheck == IS_FULL_TIME)
-
-        {
-        	empHrs = FULL_WORKING_DAY;
+		if (empcheck == IS_FULL_TIME) {
+        	empHrs = 8;
         	empWage = WAGE_PER_HR * empHrs;
         }
-        else
-        {
+		else if (empcheck == IS_Part_Time) {
+        	empHrs = 4;
+        	empWage = WAGE_PER_HR * empHrs;
+        }
+        else {
         	empHrs = 0;
         	empWage = WAGE_PER_HR * empHrs;
         }
