1295891885ceaf6bc6d70c3b70fd820c4bf3fa91
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-20 15:01:57 +0530

    Added UC2 Employee Hrs & Wage

--- EmployWage.java
+++ EmployWage.java
@@ -3,14 +3,27 @@ package com.employeeWage;
 public class EmployWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employ Wage");
-
+		
+		int WAGE_PER_HR =20; // @param WAGE_PER_HR is used to assign wage per hour
+        int FULL_WORKING_DAY =8; // @param IS_FULL_TIME is used to assign full working day time
 		int IS_FULL_TIME = 1; // @param IS_FULL_TIME is used to check job status
+		int empHrs =0;
+        int empWage = 0;
+		
 		// Computation
 		double empcheck = Math.floor(Math.random() * 10) % 2;
+		
 		if (empcheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+        {
+        	empHrs = FULL_WORKING_DAY;
+        	empWage = WAGE_PER_HR * empHrs;
+        }
+        else
+        {
+        	empHrs = 0;
+        	empWage = WAGE_PER_HR * empHrs;
+        }
+        System.out.println("Employee Wage: Rs.  " + empWage);
 	}
 
 }
