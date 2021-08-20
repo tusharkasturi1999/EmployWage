f6eac37077d24eb09ee8017cc531d226bb8c5114
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-20 14:35:35 +0530

    Added UC1 Employee Present / Not

--- EmployWage.java
+++ EmployWage.java
@@ -1,8 +1,16 @@
 package com.employeeWage;
 
 public class EmployWage {
-	public static void main( String[] args ) {
+	public static void main(String[] args) {
 		System.out.println("Welcome to Employ Wage");
+
+		int IS_FULL_TIME = 1; // @param IS_FULL_TIME is used to check job status
+		// Computation
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
