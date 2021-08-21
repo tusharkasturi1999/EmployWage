8f712b6616932ecf4ca20fc2cb1ef99641c408be
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-21 17:09:00 +0530

    Added UC8- Added variable workhours, workdays, wage per hour

--- EmployWage.java
+++ EmployWage.java
@@ -1,22 +1,36 @@
 package com.employeeWage;
 
 public class EmployWage {
-	public static final int IS_Part_Time = 1;// @param IS_Part_Time is used to check job status
-	public static final int WAGE_PER_HR = 20; // @param WAGE_PER_HR is used to assign wage per hour
-	public static final int IS_FULL_TIME = 2; // @param IS_FULL_TIME is used to check job status
-	public static final int workdays = 20;
-	public static final int workhours = 100;
 	
-	public static void calculation() {
-
+	public static final int IS_Part_Time = 1;// @param IS_Part_Time is used to check job status
+	public static final int IS_FULL_TIME = 2; // @param IS_FULL_TIME is used to check job status
+	
+	public int WAGE_PER_HR; // @param WAGE_PER_HR is used to assign wage per hour
+	public int workdays;  //@param workdays is used to assign workdays for a company
+	public int workhours; //@param workhours is used to assign workhours for a company
+	
+	/* Constructor
+		EmployWage(int WAGE_PER_HR,int workdays,int workhours){
+		this.WAGE_PER_HR = WAGE_PER_HR;
+		this.workdays = workdays;
+		this.workhours = workhours;
+	}
+	*/
+	
+	// This is the method to calculate monthly wages
+	public void calculation(String companyName,int WAGE_PER_HR,int workdays,int workhours) {
+		
+		// Initializing variables
 		int totalwage =0;
 		int empHrs = 0;
 		int empWage = 0;
 		int totalworkdays = 0;
 		int totalworkhours = 0;
-
+		
+		System.out.println(companyName);
+		
 		// Computation
-		while (totalworkdays < 20 && totalworkhours < 100) {
+		while (totalworkdays < workdays && totalworkhours < workhours) {
 			double empcheck = Math.floor(Math.random() * 10) % 3;
 
 			switch ((int) empcheck) {
@@ -45,9 +59,10 @@ public class EmployWage {
 
 			}
 			totalwage = totalwage + empWage;
-			System.out.println("Employee Wage till now: Rs.  " + totalwage);
 		}
 		System.out.println("Employee Wage per Month: Rs.  " + totalwage);
+		System.out.println();
+		System.out.println();
 
 	}
 	
@@ -55,7 +70,18 @@ public class EmployWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employ Wage");
-		calculation();
+		System.out.println();
+		System.out.println();
+		
+		//Creating onjects for each company
+		EmployWage company = new EmployWage();
+
+		
+		
+		company.calculation("TATA",30,20,100);
+		company.calculation("RELIANCE",25,25,95);
+		company.calculation("DMART",20,30,100);
+		
 	}
 
 }
